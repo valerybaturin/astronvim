2a294b4704bf8aa37958bed2f15c8d8b06899577
Валерий Батурин <Valery.Baturin@cyberprotect.ru>
2024-09-17 12:16:58 +0300

    Disable lsp-lines

--- lua/community.lua
+++ lua/community.lua
@@ -7,6 +7,7 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.color.transparent-nvim" },
   -- import/override with your plugins folder
 }
--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -7,7 +7,7 @@ return {
   },
   config = function()
     require("go").setup {
-      diagnostic = false,
+      -- diagnostic = false,
     }
   end,
   event = { "CmdlineEnter" },
